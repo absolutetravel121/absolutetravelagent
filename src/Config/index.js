041519234153397e2e08ad97ed4a028ce8c43d22
Mohd Arif <s.arifsaifi000@gmail.com>
2025-06-13 23:49:01 +0530

    change url

---
+++ src/Config/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+module.exports = {
+  googleClientId: " ",
+  calendlyLink: " ",
+  // baseUrl: process.env.NEXT_PUBLIC_SERVER_URL,
+  // baseUrl:'http://localhost:7000/api'
+  // baseUrl: "https://api.absolutetravel.co.in",
+  baseUrl: "https://api.absolutetravel.site",
+
+};
+
