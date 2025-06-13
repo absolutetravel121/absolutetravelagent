1c7f2dec4ea0286bd19fb8651eb80efa3db35a20
Mohd Arif <s.arifsaifi000@gmail.com>
2025-06-14 00:10:52 +0530

    change text

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div  >
-    <h1>Welcome to My travel agent portal</h1>
+    <h1>Welcome to My absolute travel agent portal</h1>
     </div>
   );
 }
