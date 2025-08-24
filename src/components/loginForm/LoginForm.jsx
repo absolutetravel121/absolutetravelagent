21281c609d90dda559b4e7984f2161a79a2c61af
Ayush Maheshwari <80757890+ayush645@users.noreply.github.com>
2025-08-25 01:19:18 +0530

    Update LoginForm.jsx

--- src/components/loginForm/LoginForm.jsx
+++ src/components/loginForm/LoginForm.jsx
@@ -33,7 +33,7 @@ const LoginForm = () => {
         </div>
         <div className={styles.form_container}>
           <HeadingText
-            textTitle={"Welcome, Travel Partners! Ayush Maheshwari"}
+            textTitle={"Welcome, Travel Partners!"}
             level={2}
             className={styles.heading}
           />
